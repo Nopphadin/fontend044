43dd5eba8f72373f683e730cd442a62dfd920a65
Nopphadin <onechaiya.4274@gmail.com>
2025-06-30 09:34:06 +0700

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import { Prompt } from 'next/font/google';
+import "./globals.css";
+import Navigation from "./conponents/Navigation";
+
+const prompt = Prompt({
+  subsets: ['thai', 'latin'], // รองรับภาษาไทย
+  weight: ['300', '400', '500', '600', '700'],
+  display: 'swap',
+});
+
+
+
+export const metadata = {
+  title: 'เว็บไซต์ของคุณ',
+  description: 'เว็บไซต์ที่ใช้ฟอนต์ Prompt',
+};
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="th">
+      <body className={prompt.className}>
+        <Navigation/>
+        {children}
+      </body>
+    </html>
+  );
+}
