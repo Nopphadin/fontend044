c843dc27e8dd4f81f0a36e0c974afb5a966fb9ab
Nopphadin <onechaiya.4274@gmail.com>
2025-07-07 12:38:12 +0700

    f

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
-
+import Card from "./conponents/card";
 import Carousel from "./conponents/carousel";
 export default function carousel() {
   return(
     <div>
       <carousel/>
+      <Card/>
     </div>
   );
 }
