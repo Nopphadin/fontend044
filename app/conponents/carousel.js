d4a21e74633a739dae6c34f56f052e6b8e7ae606
Nopphadin <onechaiya.4274@gmail.com>
2025-07-07 11:59:34 +0700

    f

--- app/conponents/carousel.js
+++ app/conponents/carousel.js
@@ -10,13 +10,13 @@ export default function Carousel() {
 <div id="carouselExample" className="carousel slide carousel-fade" data-bs-ride="carousel">
   <div className="carousel-inner">
     <div className="carousel-item active">
-      <Image src="https://cdn.chiangmainews.co.th/wp-content/uploads/2020/10/04115936/IMG_4046-scaled.jpg" className="d-block w-100" alt="..." width={1920} height={690} />
+      <Image src="https://www.cmtc.ac.th/wp-content/uploads/2025/03/banner-slide-ITA67-21032025.png" className="d-block w-100" alt="..." width={1920} height={690} />
     </div>
     <div className="carousel-item">
       <Image src="https://www.cmtc.ac.th/wp-content/uploads/2021/05/img01-edition.jpg" className="d-block w-100" alt="..." width={1920} height={690} />
     </div>
     <div className="carousel-item">
-      <Image src="https://www.cmvc.ac.th/main/wp-content/uploads/2024/12/2112-24.jpg" className="d-block w-100" alt="..." width={1920} height={690} />
+      <Image src="/images/sliders/sliders-03.png" className="d-block w-100" alt="..." width={1920} height={690} />
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
