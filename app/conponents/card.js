05c981907ce9d8dc1d73c50f836936db1cd96840
Nopphadin <onechaiya.4274@gmail.com>
2025-07-07 12:52:58 +0700

    f

--- app/conponents/card.js
+++ app/conponents/card.js
@@ -12,7 +12,7 @@ export default function Card() {
         <div className="row">
             <div className="col-md-4 mb-4">
                 <div className="card">
-                <img src="/images/card/card1.jpg" className="card-img-top img-responsive" alt="..." />
+                <img src="https://preview.redd.it/uswkrcp7wq791.jpg?width=1080&crop=smart&auto=webp&s=0e2694db04b0ec834e89dadaaf3f779bcc91052a" className="card-img-top img-responsive" alt="..." />
                 <div className="card-body">
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
                 </div>
@@ -21,7 +21,7 @@ export default function Card() {
 
             <div className="col-md-4 mb-4">
             <div className="card">
-                <img src="/images/card/card1.jpg" className="card-img-top" alt="..." />
+                <img src="https://img.zeit.de/digital/games/2024-06/elden-ring-shadow-erdtree-bild/wide__1000x562" className="card-img-top" alt="..." />
                 <div className="card-body">
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
                 </div>
@@ -29,7 +29,7 @@ export default function Card() {
             </div>
             <div className="col-md-4 mb-4">
             <div className="card">
-                <img src="/images/card/card1.jpg" className="card-img-top" alt="..." />
+                <img src="https://storage.googleapis.com/grunex-com/media/images/aug0vinf2rlsbjn67618b029aa1c107716464.jpe" className="card-img-top" alt="..." />
                 <div className="card-body">
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
                 </div>
