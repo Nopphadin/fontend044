58862876f855c9e8c7bec0a3e36f8e36ac9bdc6f
Nopphadin <onechaiya.4274@gmail.com>
2025-07-07 09:57:34 +0700

    1

--- app/conponents/Navigation.js
+++ app/conponents/Navigation.js
@@ -17,14 +17,14 @@ export default function Navigation() {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="#">หน้าหลัก</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="#">บริการ</a>
         </li>
         <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            เกี่ยวกับเรา
           </a>
           <ul className="dropdown-menu">
             <li><a className="dropdown-item" href="#">Action</a></li>
