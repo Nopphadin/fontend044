29869262e97a7a2fbfc30c7337b64ca178f673ca
Nopphadin <onechaiya.4274@gmail.com>
2025-07-07 12:13:13 +0700

    a

--- app/conponents/carousel.js
+++ app/conponents/carousel.js
@@ -13,10 +13,10 @@ export default function Carousel() {
       <Image src="https://www.cmtc.ac.th/wp-content/uploads/2025/03/banner-slide-ITA67-21032025.png" className="d-block w-100" alt="..." width={1920} height={690} />
     </div>
     <div className="carousel-item">
-      <Image src="https://www.cmtc.ac.th/wp-content/uploads/2021/05/img01-edition.jpg" className="d-block w-100" alt="..." width={1920} height={690} />
+      <Image src="https://www.cmtc.ac.th/wp-content/uploads/2025/07/%E0%B9%81%E0%B8%AA%E0%B8%94%E0%B8%87%E0%B8%84%E0%B8%A7%E0%B8%B2%E0%B8%A1%E0%B8%A2%E0%B8%B4%E0%B8%99%E0%B8%94%E0%B8%B5%E0%B8%A3%E0%B8%B1%E0%B8%90%E0%B8%A1%E0%B8%99%E0%B8%95%E0%B8%A3%E0%B8%B5%E0%B8%A7%E0%B9%88%E0%B8%B2%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%81%E0%B8%A3%E0%B8%B0%E0%B8%97%E0%B8%A3%E0%B8%A7%E0%B8%87%E0%B8%A8%E0%B8%B6%E0%B8%81%E0%B8%A9%E0%B8%B2%E0%B8%98%E0%B8%B4%E0%B8%81%E0%B8%B2%E0%B8%A3.png" className="d-block w-100" alt="..." width={1920} height={690} />
     </div>
     <div className="carousel-item">
-      <Image src="/images/sliders/sliders-03.png" className="d-block w-100" alt="..." width={1920} height={690} />
+      <Image src="https://www.share2trade.com/storage/Web%20Board/2024/241024/SYNEX_241024_%E0%B8%8B%E0%B8%B4%E0%B8%99%E0%B9%80%E0%B8%99%E0%B9%87%E0%B8%84%E0%B8%AF%20%E0%B8%9B%E0%B8%B1%E0%B9%89%E0%B8%99%E0%B8%8A%E0%B9%88%E0%B8%B2%E0%B8%87%20%E0%B9%80%E0%B8%97%E0%B8%84%E0%B8%99%E0%B8%B4%E0%B8%84%E0%B9%80%E0%B8%8A%E0%B8%B5%E0%B8%A2%E0%B8%87%E0%B9%83%E0%B8%AB%E0%B8%A1%E0%B9%88%20(1).jpg" className="d-block w-100" alt="..." width={1920} height={690} />
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
--- app/page.js
+++ app/page.js
@@ -1,12 +1,10 @@
-import Carousel from "./conponents/carousel"
-export default function Home() {
-  return(
-    <>
-    <h1 className="text-4xl text-center text-black-500">Home</h1>
-    <Carousel/>
 
-   
-    </>
+import Carousel from "./conponents/carousel"
+export default function carousel() {
+  return(
+    <div>
+      <carousel/>
+    </div>
   )
 }
     
