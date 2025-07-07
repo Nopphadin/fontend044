d0034bd143d8ef7d2b255cfc65bb74017c4411ba
Nopphadin <onechaiya.4274@gmail.com>
2025-07-07 09:45:35 +0700

    1

--- app/conponents/Navigation.js
+++ app/conponents/Navigation.js
@@ -4,13 +4,43 @@ import Link from 'next/link';
 
 export default function Navigation() {
   return (
-    <nav style={{ padding: '1rem', borderBottom: '1px solid #eee' }}>
-      <ul style={{ display: 'flex', gap: '1rem', listStyle: 'none' }}>
-        <li><Link href="/">หน้าแรก</Link></li>
-        <li><Link href="/about">เกี่ยวกับ</Link></li>
-        <li><Link href="/service">บริการ</Link></li>
-        <li><Link href="/contact">ติดต่อ</Link></li>
+  <nav classname="navbar navbar-expand-lg bg-body-tertiary">
+  <div classname="container-fluid">
+    <a classname="navbar-brand" href="#">Navbar</a>
+    <button classname="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span classname="navbar-toggler-icon">
+      </span></button>
+    <div classname="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul classname="navbar-nav me-auto mb-2 mb-lg-0">
+        <li classname="nav-item">
+          <a classname="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li classname="nav-item">
+          <a classname="nav-link" href="#">Link</a>
+        </li>
+        <li classname="nav-item dropdown">
+          <a classname="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul classname="dropdown-menu">
+            <li><a classname="dropdown-item" href="#">Action</a></li>
+            <li><a classname="dropdown-item" href="#">Another action</a></li>
+            <li><hr classname="dropdown-divider" /></li>
+            <li><a classname="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li classname="nav-item">
+          <a classname="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
       </ul>
-    </nav>
+      <form classname="d-flex" role="search">
+        <input classname="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button classname="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+
   );
 }
