49a1db17baedcced023cff2ac998a8ae1c96b4fb
Nopphadin <onechaiya.4274@gmail.com>
2025-07-07 10:50:31 +0700

    7

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Prompt } from 'next/font/google';
-
 import Navigation from "./conponents/Navigation";
+
 
 const prompt = Prompt({
   subsets: ['thai', 'latin'], // รองรับภาษาไทย
